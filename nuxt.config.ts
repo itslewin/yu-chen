5b32ca00833af411ac1e6e0be89861f736ccf388
Lewin Kästner <kaestner@adelphi.de>
2024-08-26 17:43:46 +0200

    add nuxt studio module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/content", "@nuxt/ui", "@nuxtjs/tailwindcss"],
+  modules: [
+    "@nuxt/content",
+    "@nuxt/ui",
+    "@nuxtjs/tailwindcss",
+    "@nuxthq/studio",
+  ],
   css: [`assets/css/main.css`],
 });
