1417ee1b8e86e75e4f5d48421302b42dd9e0af91
Lewin Kästner <kaestner@adelphi.de>
2024-10-20 17:19:04 +0200

    adapt spacing

--- components/content/ImageGallery.vue
+++ components/content/ImageGallery.vue
@@ -3,28 +3,17 @@
     <Swiper
       :modules="[SwiperFreeMode, SwiperMousewheel]"
       :slides-per-view="1"
+      :spaceBetween="20"
       :freeMode="true"
       :mousewheel="true"
     >
       <SwiperSlide v-for="slide in 9" :key="slide">
-        <NuxtImg :src="`/photos/P${slide}.jpg`" class="w-10/12 mt-3" />
+        <NuxtImg :src="`/photos/P${slide}.jpg`" />
       </SwiperSlide>
     </Swiper>
   </div>
-  <div class="block lg:hidden mb-10" v-for="slide in 9">
-    <!-- <Swiper
-      :modules="[SwiperFreeMode, SwiperMousewheel]"
-      :slides-per-view="2"
-      :freeMode="true"
-      :mousewheel="true"
-      :direction="'vertical'"
-      class="h-screen"
-    >
-      <SwiperSlide v-for="slide in 9" :key="slide">
-        <NuxtImg :src="`/photos/P${slide}.jpg`" class="w-10/12 mx-auto" />
-      </SwiperSlide>
-    </Swiper> -->
-    <NuxtImg :src="`/photos/P${slide}.jpg`" class="w-10/12 mx-auto mb-10" />
+  <div class="block lg:hidden mb-[20px] last:mb-[40px]" v-for="slide in 9">
+    <NuxtImg :src="`/photos/P${slide}.jpg`" />
   </div>
 </template>
 
